d61f292346f9ffc672834f26a7ca23c530ca47ed
gaoy <158612783@qq.com>
2024-01-23 14:24:30 +0800

    update

--- model/member.go
+++ model/member.go
@@ -4,8 +4,8 @@ import "time"
 
 type Member struct {
 	Id            uint      `json:"id" gorm:"column:id;type:bigint;primaryKey;autoIncrement;"`
-	OpenId        string    `json:"openid" gorm:"column:open_id;type:varchar(255);uniqueIndex:open_id;"`
-	NickName      string    `json:"nickName" gorm:"column:nick_name;type:varchar(255);"`
+	OpenId        string    `json:"openid" gorm:"column:open_id;type:varchar(50);uniqueIndex:open_id;"`
+	NickName      string    `json:"nickname" gorm:"column:nick_name;type:varchar(255);"`
 	AvatarUrl     string    `json:"avatarUrl" gorm:"column:avatar_url;type:varchar(255);"`
 	LastLoginIp   string    `json:"last_login_ip" gorm:"column:last_login_ip;type:varchar(255);"`
 	LastLoginTime time.Time `json:"last_login_time" gorm:"column:last_login_time;type:datetime;"`
